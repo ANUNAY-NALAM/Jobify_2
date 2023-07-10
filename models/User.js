f36ea7540d05547b80adaee50f43b65734970627
Anunay <anunaynalam@gmail.com>
2023-07-11 01:35:19 +0530

    update user

--- models/User.js
+++ models/User.js
@@ -35,12 +35,17 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function(){
+  console.log(this.modifiedPaths())
+  if(!this.isModified('Password')) return
   const salt =await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password,salt)
+
 })
 
 UserSchema.methods.createJWT =function(){
-  return jwt.sign({userId:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+  return jwt.sign({userId:this._id},process.env.JWT_SECRET,
+    {expiresIn:process.env.JWT_LIFETIME
+    })
 }
 
 UserSchema.methods.comparePassword =async function(candidatePassword){
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import connectDB from './db/connect.js'
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
+import authenticateUser from './middleware/auth.js'
 import morgan from 'morgan'
 import authRouter from './routes/authRoutes.js'
 import jobsRouter from './routes/jobsRoutes.js'
@@ -28,7 +29,7 @@ app.get('/api/v1',(req,res)=>{
 })
 
 app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/jobs',jobsRouter)
+app.use('/api/v1/jobs',authenticateUser,jobsRouter)
 
 
 app.use(notFoundMiddleware)
